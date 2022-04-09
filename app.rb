1abafc62e784250e395fc926301400224bda7cb1
fabifreire <fabitwink@hotmail.com>
2022-04-09 13:40:53 +0100

    adding require json to app.rb

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require "sinatra/json"
 
 configure :development do
   use BetterErrors::Middleware
